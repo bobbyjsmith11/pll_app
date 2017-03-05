64099adc11102796526da62230210e0943b017c1
Bobby Smith <bobbyjsmith11@gmail.com>
2017-03-05 11:07:27 -0600

    Removed importing matplotlib

--- controllers/pll_calcs.py
+++ controllers/pll_calcs.py
@@ -8,9 +8,8 @@
 from gluon.tools import Service
 service = Service(globals())
 
-import time
 import numpy as np
-import matplotlib.pylab as plt
+# import matplotlib.pylab as plt
 from scipy.optimize import curve_fit
 from scipy.interpolate import interp1d
 from scipy.signal import savgol_filter
